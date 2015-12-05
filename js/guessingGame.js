0d1257c61fc3ccfcf7e487410203dbaaf3a64c5b
Joel Darling <joel.darling@gmail.com>
2015-12-04 19:23:44 -0500

    Created submit and generate winning number functions, added jQuery to project

---
+++ js/guessingGame.js
@@ -0,0 +1,58 @@
+/* **** Global Variables **** */
+// try to elminate these global variables in your project, these are here just to start.
+
+var playersGuess,
+    winningNumber = generateWinningNumber();
+
+
+
+/* **** Guessing Game Functions **** */
+
+// Generate the Winning Number
+
+function generateWinningNumber(){
+	// add code here
+  return Math.floor(Math.random()*99) + 1;
+
+}
+
+// Fetch the Players Guess
+
+function playersGuessSubmission(){
+	// add code here
+  var guess = $('#guessInput');
+
+  playersGuess=parseInt(guess.val());
+
+  //reset text
+  guess.val('Enter a number 1-100');
+
+  console.log("players guess: "+playersGuess);
+}
+
+// Determine if the next guess should be a lower or higher number
+
+function lowerOrHigher(){
+	// add code here
+}
+
+// Check if the Player's Guess is the winning number
+
+function checkGuess(){
+	// add code here
+}
+
+// Create a provide hint button that provides additional clues to the "Player"
+
+function provideHint(){
+	// add code here
+}
+
+// Allow the "Player" to Play Again
+
+function playAgain(){
+	// add code here
+}
+
+
+/* **** Event Listeners/Handlers ****  */
